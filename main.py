1f1148d944fbf17c8839624cea30f58855e67a66
kwosku <kwosku@gmail.com>
2025-01-22 01:04:26 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import asyncio
+import logging
+import sys
+from os import getenv
+
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+from just_token import The_token
+
+TOKEN = getenv(token=The_token)
+
+# All handlers should be attached to the Router (or Dispatcher)
+
+dp = Dispatcher()
+
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message):
+    await message.answer(f"Hello!")
+
+
+@dp.message()
+async def echo_handler(message: Message):
+    try:
+        # Send a copy of the received message
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        # But not all the types is supported to be copied so need to handle it
+        await message.answer("Nice try!")
+
+
+async def main() -> None:
+    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
